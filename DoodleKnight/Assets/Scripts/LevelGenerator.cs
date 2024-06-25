13133c5f6b92e64e63533cf311b7bd334b1a3b8b
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-06-25 16:19:57 +0300

    main game done

---
+++ DoodleKnight/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms = 50;
+    public float levelWidth = 3f;
+    public float minY = 2f;
+    public float maxY = 4f;
+
+    //use this for initialization
+    void Start()
+    {
+
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}

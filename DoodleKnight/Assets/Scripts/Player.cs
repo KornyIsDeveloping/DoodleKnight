a47b9ae1bddedcd346624a44f502091a0255b673
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-06-25 15:28:13 +0300

    player jumps
    jump mechanic implemented

---
+++ DoodleKnight/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    public float movementSpeed = 10f;
+
+    Rigidbody2D rb;
+
+    float movement = 0f;
+
+    //use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
